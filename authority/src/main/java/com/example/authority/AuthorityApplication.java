601245fcf654dc8611728255ff87f8bb9a4fd145
yangjian <820335273@qq.com>
2022-03-30 14:43:51 +0800

    feat: init

---
+++ authority/src/main/java/com/example/authority/AuthorityApplication.java
@@ -0,0 +1,16 @@
+package com.example.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangj
+ */
+@SpringBootApplication
+public class AuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class, args);
+    }
+
+}
